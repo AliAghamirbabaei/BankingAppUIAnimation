b31d73eed532f8d49e54726153729c9a137663d4
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-01-23 01:01:29 +0330

    Initial Commit

---
+++ BankingAppUI/BankingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppUIApp.swift
+//  BankingAppUI
+//
+//  Created by Ali Aghamirbabaei on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
